18a37a4a336dc0830db4ed6006eb541e0a7b8b57
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2022-09-24 18:20:42 +0000

    add comment

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ APP = Flask(__name__)
 
 @APP.route('/')
 def hello_world():
-    return 'THANK YOU BABE!\nWant to tell you I have finished my projects with udacity.\nI am very gratefull to God for giving me the priviledge to meet you, what a beautiful person you are.\nI love you, I love you\n'
-
+    return 'Hello world!'
 
 
 if __name__ == '__main__':
